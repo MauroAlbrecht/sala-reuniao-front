965841d90864d5a0003b5d938af1fb535f94996d
MauroAlbrecht <mauroint@gmail.com>
2021-09-07 12:09:54 -0300

    criando componentes de base para o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailComponent } from './room-detail/room-detail.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { RoomUpdateComponent } from './room-update/room-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailComponent,
+    RoomListComponent,
+    RoomUpdateComponent
   ],
   imports: [
     BrowserModule,
